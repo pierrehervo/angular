e40d4df3c2e2a839d89202a10f410ee6e096bb78
pierrehervo <pierre.hervo@laposte.net>
2019-07-30 11:35:26 +0200

    découverte d'angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export class Pizza{
+  id: number;
+  name: string;
+  price: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pizzaparty';
+  
+  pizza: Pizza = {
+    id: 1,
+    name: '4 fromages',
+    price: 10
+  }
 }
