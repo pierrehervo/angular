c7551224af99ba112db5f30f480439ab5e4a8488
pierrehervo <pierre.hervo@laposte.net>
2019-07-31 11:41:46 +0200

    création d'un component pizzas et routerLink sur le menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,10 @@
 import { Component } from '@angular/core';
-import { Pizza } from './models/pizza.model';
-import { PizzaService } from './pizza.service';
- 
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent {
   title = 'pizza party';
-  pizzas: Pizza[];
-  selectedPizza: Pizza; 
-
-  //On injecte le service PizzaService dans le composant pour pouvoir l'utiliser avec this.pizzaService
-  constructor(private pizzaService: PizzaService) { }
-  
-  //Equivalent du document.ready
-  //Quand le composant est pret dans le dom, on récupère les pizzas
-  ngOnInit(){
-    this.pizzas = this.pizzaService.getPizzas();
-    console.log(this.pizzas);
-  }
-
-  onSelect(pizza: Pizza){
-    console.log(pizza);
-    //On modifie la propriété pizza de l'instance de appComponent
-    this.selectedPizza = pizza;
-  }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PizzaComponent } from './pizza/pizza.component';
 import { MenuComponent } from './menu/menu.component';
+import { PizzasComponent } from './pizzas/pizzas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PizzaComponent,
-    MenuComponent
+    MenuComponent,
+    PizzasComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'pizzas', component: PizzasComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
